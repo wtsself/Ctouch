74ed77a829f4bf38cb420488d0bf63a967f7decb
wts <zhuping0030606@163.com>
2025-01-31 21:55:22 +0800

    Revert "删除无用头文件"
    This reverts commit db04bd14a2e89a0a692a299702efaf1d2d243eb9.

---
+++ Dot.h
@@ -0,0 +1,8 @@
+#pragma once
+#include<iostream>
+using namespace std;
+class Dot {
+public :
+	double x_2, y_2;
+	double Addtion(double x_2, double y_2);
+};
---
+++ test_1.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<iostream>
+#include "Dot.h"
+using namespace std;
+class Cricle {
+private:
+	 double x_1, y_1;
+public:
+	void setDot(Dot do1);
+	void setx_1(double x);
+	double getx_1();
+	void sety_1(double y);
+	double gety_1();
+};
